d4f7ca803e8c3e29dc3a1c7b514901a9cb99e25d
dgocker <cer9523@gmail.com>
2026-03-05 22:28:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  optimizeDeps: {
-    include: ['@codetrix-studio/capacitor-google-auth'],
-  },
   server: {
     host: '0.0.0.0',
     port: 3000,
